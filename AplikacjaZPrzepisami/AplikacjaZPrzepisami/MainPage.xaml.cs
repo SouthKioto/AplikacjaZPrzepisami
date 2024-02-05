e9bfd562632841959758f3f7383eb52153590dce
ImColdBlooded <bolisegabartek6@gmail.com>
2024-02-05 14:38:33 +0100

    dodanie funckjonalności "add to liked" (jeszcze nie działa)

--- AplikacjaZPrzepisami/AplikacjaZPrzepisami/MainPage.xaml.cs
+++ AplikacjaZPrzepisami/AplikacjaZPrzepisami/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using AplikacjaZPrzepisami.Classes;
+using AplikacjaZPrzepisami.Pages;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -54,7 +55,47 @@ namespace AplikacjaZPrzepisami
 
         private void CheckDetails(object sender, EventArgs e)
         {
+            var button = (Button)sender;
+            Recipe recipe = (Recipe)button.BindingContext;
 
+            DetailPage details = new DetailPage(recipe);
+            Navigation.PushAsync(details);
+        }
+
+        private void DisplaySearchedRecipe(object sender, EventArgs e)
+        {
+            GetStringAndExecuteApiRequest(searchRecipes_entry.Text);
+        }
+
+        public async void GetStringAndExecuteApiRequest(string searchedRecipe)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string apiUrl = "https://api.api-ninjas.com/v1/recipe?query=" + searchedRecipe;
+
+                    client.DefaultRequestHeaders.Add("X-Api-Key", "Pq16rcwdSrz4LGV5HcaBtA==Y3C4aADP0H00T3sP");
+
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonContent = await response.Content.ReadAsStringAsync();
+                        List<Recipe> recipes = JsonConvert.DeserializeObject<List<Recipe>>(jsonContent);
+
+                        displaySearchRecipes.ItemsSource = recipes;
+                    }
+                    else
+                    {
+                        await DisplayAlert("Error", "Unable to fetch recipes", "OK");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
         }
     }
 }
---
+++ AplikacjaZPrzepisami/AplikacjaZPrzepisami/Pages/DetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using AplikacjaZPrzepisami.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AplikacjaZPrzepisami.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailPage : ContentPage
+    {
+        public DetailPage(Recipe recipe)
+        {
+            InitializeComponent();
+            Title = recipe.Title;
+            imgrediends_label.Text = recipe.Ingredients;
+            people_label.Text = recipe.Servings;
+            instruction_label.Text = recipe.Instructions;
+        }
+    }
+}
